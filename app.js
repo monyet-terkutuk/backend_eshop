75dcfcc8c66935fe1bcec4c765b72f27a8145d80
undeath-cyber <gabrielxjonathan.12@gmail.com>
2024-01-24 00:43:20 +0700

    feat(add welcome)

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ const order = require("./controller/order");
 const conversation = require("./controller/conversation");
 const message = require("./controller/message");
 const withdraw = require("./controller/withdraw");
+const welcome = require("./controller/welcome");
 
 app.use("/api/v2/user", user);
 app.use("/api/v2/conversation", conversation);
@@ -49,6 +50,7 @@ app.use("/api/v2/event", event);
 app.use("/api/v2/coupon", coupon);
 app.use("/api/v2/payment", payment);
 app.use("/api/v2/withdraw", withdraw);
+app.use("/", welcome);
 
 // it's for ErrorHandling
 app.use(ErrorHandler);
